5ce7f33ca11442e410918e0e522e1866e0b307e3
Rajesh <rajesh68@gmail.com>
2023-03-04 10:52:44 -0700

    exmaple of modular programming is added.

---
+++ module.js
@@ -0,0 +1,5 @@
+// const stat=require('./stat');
+// const  classRet=stat().sayMyname;
+// let myName= new classRet("rajesh","Updahdayaya");
+// console.log(myName.setFirstName("boby"));
+// console.log(myName.toString())
---
+++ stat.js
@@ -0,0 +1,20 @@
+// module.exports =function () {
+//     const sum=(x,y)=> x+y;
+//     let avg= data => data.reduce(sum)/data.length;
+//     let sayHi= {greeting:"hello There", reply: "how is your day going?"};
+//    class sayMyname {
+//         constructor(firstName, lastName) {
+//             this.firstName=firstName;
+//             this.lastName=lastName;
+//         }
+//        toString(){ return `my name is ${this.firstName} ${this.lastName}` }
+//         get getFirstName(){return this.firstName;}
+//          setFirstName(firstName){
+//             this.firstName=firstName;
+//             return `your first name has beeen set to ${this.firstName}`;
+//         }
+//     }
+//     // console.log(typeof  classRetrun)
+//     return {avg,sayHi,sayMyname}
+// }
+// // module.exports={toBeExportFunc};
